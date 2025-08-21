6099bb23ab6227cd816eec1d51ef4edfc3e46629
seo2021 <eunjeong0545@gmail.com>
2025-08-21 15:29:45 +0900

    js. css 수정

--- script.js
+++ script.js
@@ -12,43 +12,50 @@ document.addEventListener("DOMContentLoaded", function () {
       name: "매콤달콤 제육볶음! 🥓",
       description:
         "매콤한 양념에 볶아낸 돼지고기가 밥도둑! 스트레스 해소에도 최고예요.",
-      image: "https://source.unsplash.com/400x250/?korean-food,jeyuk-bokkeum",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUoWXVuJOIh_sv8xy4OpFfNjZZy5XPdu5OiQ&s",
     },
     {
       name: "시원~한 냉면! 🍜",
       description:
         "무더운 여름, 살얼음 동동 시원한 냉면으로 더위를 날려보세요!",
-      image: "https://source.unsplash.com/400x250/?korean-food,naengmyeon",
+      image:
+        "https://i.namu.wiki/i/s78uFdtqEXALYdRSA1HJtoubiO-Y_mfHMo7IftA2dJ6Kz8XmQFLop9qGzHSKdooKXhibh6BCedG-kAoye6y1OQ.webp",
     },
     {
       name: "든든한 된장찌개! 🍲",
       description:
         "구수하고 깊은 맛의 된장찌개로 편안하고 든든한 점심을 즐겨보세요.",
-      image: "https://source.unsplash.com/400x250/?korean-food,doenjang-jjigae",
+      image:
+        "https://i.namu.wiki/i/i6xznN10ARKDTeZ8oe1RgsO3WjqlJuOHBeM8iouRewkHX7UPthwCCo-ZYd0THeG1MwyRW0OvfYgCAG-dAk3tHg.gif",
     },
     {
       name: "바삭 촉촉 돈까스! 🍽️",
       description:
         "겉바속촉 돈까스와 달콤한 소스의 환상 조합! 남녀노소 누구나 좋아하는 메뉴.",
-      image: "https://source.unsplash.com/400x250/?japanese-food,tonkatsu",
+      image:
+        "https://i.namu.wiki/i/aN7eMJzy4XAy1yqpL3kHb41MBsSdfPjt1ZqMfDXYk6J3-je6M8dNVOMldpbxhZ-IlO9IfHXMzpZc1tVvat5IjQ.webp",
     },
     {
       name: "상큼한 샐러드 파스타! 🥗",
       description:
         "가볍고 신선하게 즐기고 싶을 때! 상큼한 드레싱의 샐러드 파스타.",
-      image: "https://source.unsplash.com/400x250/?salad,pasta",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/e/e1/Pasta_salad_closeup.JPG",
     },
     {
       name: "고소한 비빔밥! 🥣",
       description:
         "다채로운 채소와 고기, 고추장이 어우러진 건강하고 맛있는 한 끼!",
-      image: "https://source.unsplash.com/400x250/?korean-food,bibimbap",
+      image:
+        "https://i.namu.wiki/i/dgjXU86ae29hDSCza-L0GZlFt3T9lRx1Ug9cKtqWSzMzs7Cd0CN2SzyLFEJcHVFviKcxAlIwxcllT9s2sck0RA.jpg",
     },
     {
       name: "뜨끈한 순대국밥! 🥘",
       description:
         "속이 든든하고 뜨끈한 순대국밥으로 얼어붙은 몸을 녹여보세요.",
-      image: "https://source.unsplash.com/400x250/?korean-food,sundaegukbap",
+      image:
+        "https://i.namu.wiki/i/w4fN6GjXus8MSVxfYNDBtsBQZeArMnz0GrRLzftISNw_woXS3Z5KIJqFSG1GfLFGKQ97LxNGmNwHo19j1cWBOg.webp",
     },
   ];
 
